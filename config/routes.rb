ae7e7295f40f180b506371d685785ec43c7bd811
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-08-27 17:39:14 -0500

    feat: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,19 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  constraints subdomain: 'api' do
+    namespace :api, path:'/' do
+      with_options only: [:index, :create, :new, :edit, :show, :update, :destroy] do |list_only|
+        list_only.resources :post_contexts
+        # list_only.resources :users
+        # list_only.resources :posts
+        # list_only.resources :post_categories
+        # list_only.resources :type_goals
+        # list_only.resources :statisctics
+        # list_only.resources :goals
+        # list_only.resources :projects
+        # list_only.resources :payments
+        # list_only.resources :events
+        # list_only.resources :groups
+      end
+    end
+  end
 end

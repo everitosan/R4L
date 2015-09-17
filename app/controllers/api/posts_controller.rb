11480b0dd86d0dba7cdf4c6eb5cc7490f5c9daa2
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-09-17 16:11:46 -0500

    react: Post response

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,25 @@ module API
   class PostsController < ApiController
 
     def index
-      gIndex(Post)
+      @posts = Post.all
+
+      @postResponse = []
+
+      @posts.each do |post|
+        @postResponse.push({
+          :id => post.id,
+          :user=> post.user.name,
+          :userPic=> post.user.picProfile,
+          :text => post.text,
+          :image => post.image,
+          :likes => post.likes,
+          :category => post.post_category.name
+        })
+      end
+
+      respond_to do |format|
+        format.json { render json:@postResponse, status: :ok }
+      end
     end
 
     def create

f075dd49cd1128161b85208fad36817b4eac1292
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-09-19 11:32:52 -0500

    feat: js filter for dates

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,22 @@
+(function(window){
+  var filter = {};
+  filter.montify =  function (data) {
+      var months = {
+        "01": "Enero",
+        "02": "Febrero",
+        "03": "Marzo",
+        "04": "Abril",
+        "05": "Mayo",
+        "06": "Junio",
+        "07": "Julio",
+        "08": "Agosto",
+        "09": "Septiembre",
+        "10": "Octubre",
+        "11": "Noviembre",
+        "12": "Diciembre"
+      };
+    return months[data];
+  };
+
+  window.filter = filter;
+})(window);
